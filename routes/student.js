b1ad061ca694c04c212033a56729dcdc4b299871
Charif Chatreewong <master-charif@outlook.com>
2024-08-19 21:46:25 +0700

    add middleware
    + timestamp middleware

--- routes/student.js
+++ routes/student.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { getStudentById, getAllStudent, insertStudent, updateStudent, deleteStudent } = require('../controllers/student');
 
+const timeStamp = (req, res, next) => {
+    const time = Date.now();
+    const readableTime = new Date(time);
+    console.log('Request Time :', readableTime.toString());
+    console.log('Method :', req.method);
+    console.log('Path : /api/student' + req.path);
+    next();
+}
+
+router.use(timeStamp);
+
 router.get('/:id', getStudentById);
 router.get('/', getAllStudent);
 router.post('/', insertStudent);
